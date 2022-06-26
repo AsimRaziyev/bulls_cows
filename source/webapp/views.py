7e10715d987bb91cc467ff40d1d76fa39bc732d0
Assym <asimo_555@mail.ru>
2022-06-27 03:33:06 +0600

    2 коммит функция генерировать_числа

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from random import shuffle
 
+
+def generate_numbers(n):
+    data = list(range(1, 10))
+    shuffle(data)
+    res = data[:n]
+    print(res)
+    return res
+
+
+secret = generate_numbers(4)
+actual = []
 
 
 def sum_calc(request):
@@ -9,5 +22,6 @@ def sum_calc(request):
         number_1 = request.POST.get('number')
         list1 = list(number_1.split())
         list2 = list(map(int, list1))
+        global actual
         actual = list2
 
